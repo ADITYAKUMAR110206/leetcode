dd43baadcc11461c254049bf2d8d225c3bb5d92a
ADITYAKUMAR110206 <148384414+ADITYAKUMAR110206@users.noreply.github.com>
2026-03-21 10:33:22 +0530

    Time: 7 ms (93.77%) | Memory: 13.6 MB (40.55%) - LeetSync

---
+++ 126-word-ladder-ii/word-ladder-ii.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        vector<vector<string>> result;
+        if (!dict.count(endWord)) return result;
+
+        unordered_map<string, vector<string>> parents;
+        unordered_set<string> current, next;
+        current.insert(beginWord);
+        bool found = false;
+
+        while (!current.empty() && !found) {
+            for (auto &w : current) dict.erase(w);
+            for (auto &word : current) {
+                string temp = word;
+                for (int i = 0; i < temp.size(); i++) {
+                    char original = temp[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        temp[i] = c;
+                        if (dict.count(temp)) {
+                            next.insert(temp);
+                            parents[temp].push_back(word);
+                            if (temp == endWord) found = true;
+                        }
+                    }
+                    temp[i] = original;
+                }
+            }
+            current = next;
+            next.clear();
+        }
+
+        if (!found) return result;
+
+        vector<string> path = {endWord};
+        function<void(string)> dfs = [&](string word) {
+            if (word == beginWord) {
+                vector<string> temp = path;
+                reverse(temp.begin(), temp.end());
+                result.push_back(temp);
+                return;
+            }
+            for (auto &p : parents[word]) {
+                path.push_back(p);
+                dfs(p);
+                path.pop_back();
+            }
+        };
+
+        dfs(endWord);
+        return result;
+    }
+};
